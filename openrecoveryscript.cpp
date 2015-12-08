29701a774e6a6c247cf19ef0468c6d1b4b144e00
James Golovich <james@gnuinter.net>
2015-12-08 09:26:11 -0800

    Allow OpenRecovery script to append to file

--- openrecoveryscript.cpp
+++ openrecoveryscript.cpp
@@ -417,10 +417,10 @@ int OpenRecoveryScript::run_script_file(void) {
 }
 
 int OpenRecoveryScript::Insert_ORS_Command(string Command) {
-	ofstream ORSfile(SCRIPT_FILE_TMP);
+	ofstream ORSfile(SCRIPT_FILE_TMP, fstream::app);
 	if (ORSfile.is_open()) {
-		//if (Command.substr(Command.size() - 1, 1) != "\n")
-		//	Command += "\n";
+		if (Command.substr(Command.size() - 1, 1) != "\n")
+			Command += "\n";
 		LOGINFO("Inserting '%s'\n", Command.c_str());
 		ORSfile << Command.c_str();
 		ORSfile.close();
